61165eb792c5d770fe8f84c68bacd94c0ad5d4e8
Fin <lockertfin@gmail.com>
2025-02-04 03:23:07 -0600

    Tiny edits to Fish script

--- First Game/Assets/Scenes/Scripts/Fish.cs
+++ First Game/Assets/Scenes/Scripts/Fish.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class Fish : MonoBehaviour
 {
+
+    private Rigidbody2D rb2d;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -14,4 +17,3 @@ public class Fish : MonoBehaviour
 
     }
 }
-
