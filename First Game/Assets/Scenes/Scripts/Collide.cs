d4a8ca1fdf80c1621fb56787dd2ae0ae58b925ce
Fin <lockertfin@gmail.com>
2025-02-04 20:37:23 -0600

    Fish are destroyed when they hit the basket

---
+++ First Game/Assets/Scenes/Scripts/Collide.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Collide : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        // Fish hits basket
+        if (col.gameObject.name == "Fish" || col.gameObject.name == "Fish(Clone)")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
