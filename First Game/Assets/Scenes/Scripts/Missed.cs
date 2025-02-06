ea896ccb699d51082b4824e13131082c0515de54
Fin <lockertfin@gmail.com>
2025-02-06 17:30:42 -0600

    Cat hisses when you miss a fish (less annoying)

--- First Game/Assets/Scenes/Scripts/Missed.cs
+++ First Game/Assets/Scenes/Scripts/Missed.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class Missed : MonoBehaviour
 {
+
+    private AudioSource audioSource;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -22,5 +25,6 @@ public class Missed : MonoBehaviour
             Destroy(col.gameObject);
         }
         GameManager.Instance.missScore(1);
+        audioSource.Play();
     }
 }
