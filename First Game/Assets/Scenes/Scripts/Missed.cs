29d5875b37ea152cb69a02b72911e335433bdb1a
Fin <lockertfin@gmail.com>
2025-02-05 19:57:04 -0600

    Fish are now destroyed on contact with ground (instead of falling through)

---
+++ First Game/Assets/Scenes/Scripts/Missed.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Missed : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        // Fish hits ground
+        if (col.gameObject.name == "Fish" || col.gameObject.name == "Fish(Clone)") // Source 1
+        {
+            Destroy(col.gameObject);
+        }
+        // GameManager.Instance.incScore(1);
+    }
+}
